06ec9524c5b81bfb81370a62a8835218219accea
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-09 12:24:33 +0200

    generate a static seed of ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +9,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks = open(url).read
+ingredients = JSON.parse(drinks)
+
+ingredients_array = ingredients["drinks"]
+
+ingredients_array.each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
