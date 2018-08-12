191109b565e0cc045646d8a270b17c0a45b8939d
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-12 17:34:21 +0200

    Add validation, presence true for photo

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,6 +4,6 @@ class Cocktail < ApplicationRecord
   has_many :doses
   has_many :ingredients, through: :doses, dependent: :destroy
 
-  validates :name, uniqueness: true
+  validates :name, :photo, uniqueness: true
   validates :name, presence: true
 end
